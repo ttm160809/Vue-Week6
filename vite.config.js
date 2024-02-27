73fa82aea5b22bd9c34180d4938645cff12a762b
Yen <ttm160809@gmail.com>
2024-02-27 22:33:27 +0900

    Ver0227_2

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,8 @@ import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? `/${process.env.REPOSITORY_NAME}/` : '/',
+  base: '/Vue-Week6/',
+  // base: process.env.NODE_ENV === 'production' ? `/${process.env.REPOSITORY_NAME}/` : '/',
   plugins: [
     vue(),
     eslintPlugin({
